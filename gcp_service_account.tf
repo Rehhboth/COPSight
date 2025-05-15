1c52a603ec7acffa0930bd94cd6d335889298022
Arjun Anand <arjunwdr@gmail.com>
2025-05-15 22:15:12 +0100

    updated service account

---
+++ gcp_service_account.tf
@@ -0,0 +1,11 @@
+[gcp_service_account]
+type                    = "service_account"
+project_id              = "copsight"
+private_key_id          = "4efa873f8c4938234880d974c186620985a94541"
+private_key             = "-----BEGIN PRIVATE KEY-----,MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCbaGb6CX9BKsGs,JGz879O+nb0NvXOoUnd8y7UIRA5YqcOUJWF1H8zJUXYrTnwGh6+vrDsd1dtoP5P7,u4VpW7ScMUELCUecma94CXCEqv+0awPObrcJNpUwia3FNMSuSPPMMjganRYu4dYK,pL5M8BdGs08I0NQ5isZp5VQqqRa7SGcoEhyAPFJJ0RkeKqXPjXPzbbzVUE4/x43K,ZDdNozdLlZ4U9NcS8E58Y5G/8EoIe/Oy7sqtRp4w8sqSrI0wc3yELJTA2zeJagoH,FiuW5llY28eDpaIf/fa+tNbeZ/RorNS96TYJmhtE3DbWOGAuqzZ+JjQnzhxtgrIY,MCRFI1qVAgMBAAECggEAItv50VdFBKyfEcp/6gKvx/+g8w6OfXC2Hbeh7W86mpfF,55q08C1IZdYQgClUN7DC71BeWFRqqUktnBdVlyr7adfAg8W6mv18NxxC8Xdb8S/a,BDmgdAAa2e7gHC64eXGiwih3njyjR7Pfq7aFe0OVrQW2kj8pg5jF5eYLiaAxGaQL,8Yn3SqtjEsnXmnCbJe9fQ5KBCFXVPh5E5VyNybTmARavSje7L5JhM4UbwoJXbD0Y,Q2qrkGufN0Jl525a/mv6vDJHYbYcSFWaJU4Ft23O32jGR3Imstq3d7CDe2hJK8e7,m/bq4wtiIakEdzdA+TzcnlBPDJ9wXrBxOIIMW67q9wKBgQDQgE3duh8bfV/Jce5i,Uif+Jsm8zQkmFYpbd4T3ip7W2kCCqs8vkF2mgwqsrvsqzblQoTmNjCqKUTgn9M5n,ohZmXV6iTTjf3vTznJLIq0DQKBQsZeapq1/YSt8fEZA5zwCMMO4NZuNzGCtVH6vz,4AgxHU/GMADE40sj/KIb34JNUwKBgQC+z7d7wbSF+FHRt+qzrmQtPkyw+4qrytoD,RI7ubp+xHXGx5NkwgynwUuL1tCDgdUE/h0YH6wsjyLbGbHD1uz2tTibqbrttjoJi,P6LYWLHrxP/lBNp0480bJTfNK0I+GFaN6tyL,TYTqED11g807hmT/SlWMzl5De/,jEPBDhLTdwKBgHEBGTLTbYi4qvHDjjA71vEolK5/cjXx9sscPDTuLKjGIbCwYxqk,ZB2baG2vaPIKDiZlO4w8K9JLsuP1CumgMAPLFz9gGzM65AglFGkuaDJphoWJtTrA,Z3ITIlr0afbq+qEA5ATffuciclnwkIHdKpN0D4ia+8+6YT9f7Xyt5zGBAoGATqAP,DkrSVUXuiQlNB/87Pj1moqL0EDgFP8V3VB53ZScp21DeghP6KxIsfWxdA8CtQz09,iTO8ImGplvfGUCt4jbofZQh96PYJGez48EcUAX/qpfcrAXGA6cebR8qkgCvNNqtp,d7JBeYRk6s1IicAAQwB9YcjGnZA9vItSu/FNpp8CgYEAha8u1cX2iw0p3GGFrisO,mw+CTbjEZrYwndfdMY2ig82DRkxxoriVqvw+5k2uPDlM05jYQjVSTsLtdDY6lp9V,1CkfLIbk1XAC/1SpFAvzuvfRKVAc7aqbxEVVgGzwzA4mqgsot5YsnWDohxrrXM62,qY166tHyOa4LEZ+WzjCqXfk=,-----END PRIVATE KEY-----,"
+client_email            = "copsight@copsight.iam.gserviceaccount.com"
+client_id               = "103010424671725235716"
+auth_uri                = "https://accounts.google.com/o/oauth2/auth"
+token_uri               = "https://oauth2.googleapis.com/token"
+auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
+client_x509_cert_url    = "https://www.googleapis.com/robot/v1/metadata/x509/copsight%40copsight.iam.gserviceaccount.com" 
